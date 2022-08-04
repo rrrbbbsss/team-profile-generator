d84ff6ad4cb4c58ed7b9c7586437a75f71e065f8
Royce Strange <roycest@swbell.net>
2022-08-04 10:06:41 -0500

    genTemplate code and tests

---
+++ __tests__/genTemplate.test.js
@@ -0,0 +1,167 @@
+const Intern = require('../lib/Intern');
+const Manager = require('../lib/Manager');
+const Engineer = require('../lib/Engineer');
+jest.mock('../lib/Intern');
+jest.mock('../lib/Manager');
+jest.mock('../lib/Engineer');
+const genTemplate = require('../src/genTemplate');
+
+
+
+
+test("validate making an intern card", () => {
+    const intern = new Intern();
+    const result = genTemplate.htmlTemplate([intern]);
+
+    expect(result).toBe(genTemplate.top + 
+`
+<div class="card col-3 m-3 p-0">
+    <div class="card-header bg-primary text-white">
+        <h2 class="card-title bg-primary text-white">Bob</h2>
+        <h3 class="card-subtitle bg-primary">Intern</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">
+                Id: 2
+            </li>
+            <li class="list-group-item">
+                Email: <a href="mailto:blah@blah.com">blah@blah.com</a>
+            </li>
+            <li class="list-group-item">
+                School: blah
+            </li>
+        </ul>
+    </div>
+</div>
+` + genTemplate.bottom)
+})
+
+
+test("validate making an engineer card", () => {
+    const engineer = new Engineer();
+    const result = genTemplate.htmlTemplate([engineer]);
+
+    expect(result).toBe(genTemplate.top + 
+`
+<div class="card col-3 m-3 p-0">
+    <div class="card-header bg-primary text-white">
+        <h2 class="card-title bg-primary text-white">Bob</h2>
+        <h3 class="card-subtitle bg-primary">Engineer</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">
+                Id: 2
+            </li>
+            <li class="list-group-item">
+                Email: <a href="mailto:blah@blah.com">blah@blah.com</a>
+            </li>
+            <li class="list-group-item">
+                GitHub: <a href="https://github.com/blah">blah</a>
+            </li>
+        </ul>
+    </div>
+</div>
+` + genTemplate.bottom)
+})
+
+test("validate making a manager card", () => {
+    const manager = new Manager();
+    const result = genTemplate.htmlTemplate([manager]);
+
+    expect(result).toBe(genTemplate.top + 
+`
+<div class="card col-3 m-3 p-0">
+    <div class="card-header bg-primary text-white">
+        <h2 class="card-title bg-primary text-white">Bob</h2>
+        <h3 class="card-subtitle bg-primary">Manager</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">
+                Id: 2
+            </li>
+            <li class="list-group-item">
+                Email: <a href="mailto:blah@blah.com">blah@blah.com</a>
+            </li>
+            <li class="list-group-item">
+                Office number: 10
+            </li>
+        </ul>
+    </div>
+</div>
+` + genTemplate.bottom)
+})
+
+test("validate making a list of cards", () => {
+    const intern = new Intern();
+    const engineer = new Engineer();
+    const manager = new Manager();
+    const result = genTemplate.htmlTemplate([intern,engineer,manager]);
+
+    expect(result).toBe(genTemplate.top + 
+`
+<div class="card col-3 m-3 p-0">
+    <div class="card-header bg-primary text-white">
+        <h2 class="card-title bg-primary text-white">Bob</h2>
+        <h3 class="card-subtitle bg-primary">Intern</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">
+                Id: 2
+            </li>
+            <li class="list-group-item">
+                Email: <a href="mailto:blah@blah.com">blah@blah.com</a>
+            </li>
+            <li class="list-group-item">
+                School: blah
+            </li>
+        </ul>
+    </div>
+</div>
+` + `
+
+<div class="card col-3 m-3 p-0">
+    <div class="card-header bg-primary text-white">
+        <h2 class="card-title bg-primary text-white">Bob</h2>
+        <h3 class="card-subtitle bg-primary">Engineer</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">
+                Id: 2
+            </li>
+            <li class="list-group-item">
+                Email: <a href="mailto:blah@blah.com">blah@blah.com</a>
+            </li>
+            <li class="list-group-item">
+                GitHub: <a href="https://github.com/blah">blah</a>
+            </li>
+        </ul>
+    </div>
+</div>
+` + `
+
+<div class="card col-3 m-3 p-0">
+    <div class="card-header bg-primary text-white">
+        <h2 class="card-title bg-primary text-white">Bob</h2>
+        <h3 class="card-subtitle bg-primary">Manager</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">
+                Id: 2
+            </li>
+            <li class="list-group-item">
+                Email: <a href="mailto:blah@blah.com">blah@blah.com</a>
+            </li>
+            <li class="list-group-item">
+                Office number: 10
+            </li>
+        </ul>
+    </div>
+</div>
+` + genTemplate.bottom)
+})
---
+++ src/genTemplate.js
@@ -0,0 +1,72 @@
+const Manager = require('../lib/Manager');
+const Engineer = require('../lib/Engineer');
+const Intern = require('../lib/Intern');
+
+// using binary operators since if is not an expression
+const cardTemplate = (employee) => {
+    return `
+<div class="card col-3 m-3 p-0">
+    <div class="card-header bg-primary text-white">
+        <h2 class="card-title bg-primary text-white">${employee.getName()}</h2>
+        <h3 class="card-subtitle bg-primary">${employee.getRole()}</h3>
+    </div>
+    <div class="card-body">
+        <ul class="list-group">
+            <li class="list-group-item">
+                Id: ${employee.getId()}
+            </li>
+            <li class="list-group-item">
+                Email: <a href="mailto:${employee.getEmail()}">${employee.getEmail()}</a>
+            </li>
+            <li class="list-group-item">
+                ${(employee instanceof Manager &&
+            "Office number: " + employee.getOfficeNumber())
+        ||
+        (employee instanceof Engineer &&
+            'GitHub: <a href="https://github.com/' + employee.getGithub() + '">' + employee.getGithub() + '</a>')
+        ||
+        (employee instanceof Intern &&
+            'School: ' + employee.getSchool())}
+            </li>
+        </ul>
+    </div>
+</div>
+`
+}
+
+
+
+const top = `<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.0/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-gH2yIJqKdNHPEq0n4Mqa/HGKIhSkIHeL5AyhkYV8i59U5AR6csBvApHHNl/vI1Bx" crossorigin="anonymous">
+    <link href="./style.css" rel="stylesheet">
+    <title>Team Profile</title>
+</head>
+
+<body>
+    <header class="bg-danger text-white p-5 text-center">
+        <h1>My Team</h1>
+    </header>
+    <main class="container">
+        <section class="row justify-content-evenly">
+`
+
+const bottom = `</section>
+    </main>
+
+</body>
+
+</html>
+`
+const htmlTemplate = (data) => {
+    return top + data.map((x) => cardTemplate(x))
+            .join('\n') + bottom;
+};
+
+module.exports = { htmlTemplate, top, bottom};
