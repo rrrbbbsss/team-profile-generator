28f80261adbfe6c4f2b30960caa184f091852cfb
Royce Strange <roycest@swbell.net>
2022-08-04 06:49:10 -0500

    add utility.js

---
+++ src/utils.js
@@ -0,0 +1,4 @@
+// good enough regex for email validation: https://emailregex.com
+const emailRegex = /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+
+module.exprts = { emailRegex };
